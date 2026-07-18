4be434b861b5daafc6c37e69e485451931fa7d8d
David Arias Vázquez <dav.surf@gmail.com>
2026-07-19 00:54:07 +0200

    refactor: Rename variables in retrieval_methods_example.py
    This is just an example from the Docs, but it is done to improve Code Quality reports in GitHub

--- docs_src/configuration/retrieval_methods_example.py
+++ docs_src/configuration/retrieval_methods_example.py
@@ -1,6 +1,6 @@
-value = Config.get("app.debug")
-value = config_repository.get("app.debug")
+value1 = Config.get("app.debug")
+value2 = config_repository.get("app.debug")
 
 # Retrieve a default value if the configuration value does not exist.
-value = Config.get("app.debug", True)
-value = config_repository.get("app.debug", True)
+value3 = Config.get("app.debug", True)
+value4 = config_repository.get("app.debug", True)
