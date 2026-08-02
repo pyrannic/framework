5ab32802956de8f31399f849c40cee209bd90070
David Arias Vázquez <dav.surf@gmail.com>
2026-08-02 16:05:43 +0200

    feat: Enhance get_generic_type method and add get_generic_type method

--- tests/unit/support/reflection/test_get_generic_type.py
+++ tests/unit/support/reflection/test_get_generic_type.py
@@ -1,9 +1,6 @@
 from typing import Generic, TypeVar
 
-import pytest
-
 from pyrannic.support.reflection import get_generic_type
-
 
 T = TypeVar("T")
 
@@ -20,9 +17,18 @@ class __MyClass(__MyGenericClass[__MyTypeClass]):
     pass
 
 
+class __MyDeepClass(__MyClass):
+    pass
+
+
 def test_get_generic_type__with_instance():
     """Test the get_generic_type function with a generic class instance."""
     assert get_generic_type(__MyClass()) is __MyTypeClass
+
+
+def test_get_generic_type__with_instance_deep_inheritance():
+    """Test the get_generic_type function with a generic class instance deep in the inheritance chain."""
+    assert get_generic_type(__MyDeepClass()) is __MyTypeClass
 
 
 def test_get_generic_type__with_class():
@@ -30,13 +36,19 @@ def test_get_generic_type__with_class():
     assert get_generic_type(__MyClass) is __MyTypeClass
 
 
-def test_get_generic_type__value_error():
-    """Test the get_generic_type function with a generic class that has no generic type."""
+def test_get_generic_type__with_class_deep_inheritance():
+    """Test the get_generic_type function with a generic class deep in the inheritance chain."""
+    assert get_generic_type(__MyDeepClass) is __MyTypeClass
 
-    with pytest.raises(ValueError) as exc_info:
-        get_generic_type(__MyTypeClass)
 
-    error = str(exc_info.value)
+def test_get_generic_type__with_not_subclass():
+    """Test the get_generic_type function with a class that has no parent classes."""
+    assert get_generic_type(__MyTypeClass) is None
 
-    assert error.startswith("Generic type not found for")
-    assert error.endswith("at index 0")
+
+def test_get_generic_type__with_orig_class_attr():
+    """Test the get_generic_type function with a class that has the __orig_class__ attribute."""
+    instance = __MyTypeClass()
+    setattr(instance, "__orig_class__", __MyGenericClass[__MyTypeClass])
+
+    assert get_generic_type(instance) is __MyTypeClass
