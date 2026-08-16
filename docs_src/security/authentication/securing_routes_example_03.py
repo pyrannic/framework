e779315f2c57cf5ae9c9e944c2a8e9e4fe00342d
David Arias Vázquez <dav.surf@gmail.com>
2026-08-16 23:43:32 +0200

    docs: Add more content to the Authentication page

--- docs_src/security/authentication/securing_routes_example_03.py
+++ docs_src/security/authentication/securing_routes_example_03.py
@@ -1,8 +1,11 @@
+from pyrannic.auth import Authenticate
+
+
 @router.get(
     "/posts",
     summary="Posts Endpoint",
     description="Endpoint to retrieve the list of posts.",
-    dependencies=[Authenticate()],
+    dependencies=[Authenticate(allow_guests=True)],
 )
 async def index():
     pass
