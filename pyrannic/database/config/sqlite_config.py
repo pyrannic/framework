923dd0afad9f0e13a769a3daab21981eefbe43b3
David Arias Vázquez <dav.surf@gmail.com>
2026-09-03 17:57:15 +0200

    fix: Remove unused database connection parameters from SqliteConfig

--- pyrannic/database/config/sqlite_config.py
+++ pyrannic/database/config/sqlite_config.py
@@ -7,20 +7,8 @@ class SqliteConfig(DatabaseConfig):
     driver: str = Field(default="sqlite")
     """The database driver to use."""
 
-    host: str = Field(default="localhost")
-    """The database host to connect to."""
-
-    port: int = Field(default=3306)
-    """The database port to connect to."""
-
     database: str = Field(default="database/database.sqlite")
     """File path for SQLite database."""
-
-    username: str = Field(default="")
-    """The username to use for database authentication."""
-
-    password: str = Field(default="")
-    """The password to use for database authentication."""
 
     url: str | None = Field(default=None)
     """The database connection URL. If provided, it will override the other connection parameters."""
