12cb3d04b2362828eb30c5d698178271980d0a7e
David Arias Vázquez <dav.surf@gmail.com>
2026-09-16 07:10:01 +0200

    feat: Enhance SQLAlchemy configuration with pool settings

--- pyrannic/orm/sqlalchemy/config.py
+++ pyrannic/orm/sqlalchemy/config.py
@@ -1,4 +1,5 @@
 from pydantic import Field
+from sqlalchemy import Pool
 
 from pyrannic.config.configuration import Configuration
 
@@ -10,11 +11,17 @@ class SQLAlchemyConfig(Configuration):
     echo: bool = Field(default=False)
     """Whether to enable SQLAlchemy echo for logging SQL statements."""
 
+    poolclass: type[Pool] | None = Field(default=None)
+    """The SQLAlchemy pool class to use for connections."""
+
     pool_size: int | None = Field(default=None)
     """The size of the SQLAlchemy connection pool."""
 
     pool_pre_ping: bool = Field(default=True)
     """Whether to enable SQLAlchemy's pool pre-ping feature."""
+
+    pool_recycle: int = Field(default=-1)
+    """The number of seconds after which a connection is recycled."""
 
     max_overflow: int | None = Field(default=None)
     """The maximum number of connections that can be created beyond the pool_size."""
